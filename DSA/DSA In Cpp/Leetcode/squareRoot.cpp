2fbc6ebfc371a08242a32ab0d6e52b08d3fdd0d2
rakeshAtWork <153895722+rakeshAtWork@users.noreply.github.com>
2024-01-19 23:58:42 +0530

    Square root using Binary Search imp

---
+++ DSA/DSA In Cpp/Leetcode/squareRoot.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+   unsigned long mySqrt(int x) {
+
+       if (x == 0 || x == 1)
+            return x;
+   unsigned long ans =0;
+    int i = 1;
+    int j = x;
+    unsigned long mid = -1;
+
+    while(i<=j){
+        mid = i+(j-i)/2;
+        //unsigned long sq = mid*mid;
+        if( mid*mid >x){
+            j= mid - 1;
+            
+        }else if( mid*mid ==x){
+          return mid;
+        }else{
+            i = mid + 1;
+        }
+
+    }
+
+    return j;
+    
+    }
+
+    
+
+    
+};
