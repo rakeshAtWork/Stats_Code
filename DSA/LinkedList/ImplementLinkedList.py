5c82c1a54a621ffe70cf378bfdddffdcd5a82b96
rakeshAtWork <153895722+rakeshAtWork@users.noreply.github.com>
2024-03-13 09:21:40 +0530

    linked list code added

--- DSA/LinkedList/ImplementLinkedList.py
+++ DSA/LinkedList/ImplementLinkedList.py
@@ -1,3 +1,20 @@
 # Here we will implement linkedlist 
 
 
+# inserting node at beging will take constant time complexity.
+
+# operation becomes so easy and time complexity will be very geniune.
+
+
+# here is the code for linkedlist 
+
+
+class Node:
+    def __init__(self,data) -> None:
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+        
