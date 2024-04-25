e465c6a685ef3a77534476b0ea8539556c74e01e
rakeshAtWork <153895722+rakeshAtWork@users.noreply.github.com>
2024-04-25 14:33:15 +0530

    Changes in the C# sysconfig

--- DotNet/1. C#(cSharp)/obj/Debug/net7.0/1. C#(cSharp).AssemblyInfo.cs
+++ DotNet/1. C#(cSharp)/obj/Debug/net7.0/1. C#(cSharp).AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1. C#(cSharp)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a039be491168d54311bdf8bfb74ee4829cc49fb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b288652d9c672db00475a80f4b57c0ae1e4470d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("1. C#(cSharp)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1. C#(cSharp)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
