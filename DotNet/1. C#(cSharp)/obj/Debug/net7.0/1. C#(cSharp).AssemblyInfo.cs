9035c46c1bb7a621c4cc5b81812a6d788bce4f52
rakeshAtWork <153895722+rakeshAtWork@users.noreply.github.com>
2024-05-11 18:28:15 +0530

    Just trying to create a new Project structure for the Ml Models.
    Here we have just added the environment for the study purpose but
    
    we have to do it shaperately.

--- DotNet/1. C#(cSharp)/obj/Debug/net7.0/1. C#(cSharp).AssemblyInfo.cs
+++ DotNet/1. C#(cSharp)/obj/Debug/net7.0/1. C#(cSharp).AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1. C#(cSharp)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b288652d9c672db00475a80f4b57c0ae1e4470d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e465c6a685ef3a77534476b0ea8539556c74e01e")]
 [assembly: System.Reflection.AssemblyProductAttribute("1. C#(cSharp)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1. C#(cSharp)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
